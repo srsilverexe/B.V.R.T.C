b960f927d33393946dfd9b86f220619d707e11a1
Sr.エドワード (Sr.51LV3R) <64551686+Eduardo-de-Oliveira-Santos@users.noreply.github.com>
2021-03-15 12:40:18 -0300

    Second Commit

--- main.cs
+++ main.cs
@@ -2,47 +2,57 @@ using System;
 
 class HelloWorld
 {
-	static void Main()
-	{
-        string resposta;
-        string resposta2;
-    	double total_points;
-    	double your_points;
-    	double rest_points;
-    	double total_hours;
-    	
-    	Console.WriteLine("Bem-vindo(a) a calculadora automática para viewership rewards");
-        Console.WriteLine("Podemos começar? y/n");
-        resposta = Console.ReadLine();
+  static void Main()
+  {
+      string resposta;
+      string resposta2;
+      double total_points;
+      double your_points;
+      double rest_points;
+      double total_hours;
+      
+      Console.WriteLine("         _|_|_|                _|      _|              _|_|_|                _|_|_|_|_|                _|_|_|  ");
+      Console.WriteLine("         _|    _|              _|      _|              _|    _|                  _|                  _|        ");
+      Console.WriteLine("         _|_|_|                _|      _|              _|_|_|                    _|                  _|        ");
+      Console.WriteLine("         _|    _|                _|  _|                _|    _|                  _|                  _|        ");
+      Console.WriteLine("         _|_|_|        _|          _|          _|      _|    _|      _|          _|          _|        _|_|_|  ");
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("Começar - 1");
+      Console.WriteLine("Sair - 2");
+      Console.WriteLine("");
+      resposta = Console.ReadLine();
+      Console.Clear();
+      if (resposta == "1")
+      {
+        Console.WriteLine("Quantos pontos precisam para você pegar todos os prêmios?");
+        total_points = Convert.ToDouble(Console.ReadLine());
         Console.Clear();
-        if (resposta == "y")
+        Console.WriteLine("Quantos pontos voce ja tem?");
+        your_points = Convert.ToDouble(Console.ReadLine());
+        rest_points = total_points - your_points;
+        total_hours = rest_points / 60;
+        Console.Clear();
+        Console.WriteLine("Ok eu analisei tudo e");
+        Console.WriteLine("Ainda faltam " + rest_points + " pontos");
+        Console.WriteLine("Ainda faltam " + total_hours + " horas");
+        Console.WriteLine("voltar para o menu - 1");
+        Console.WriteLine("Sair - 2");
+        resposta2 = Console.ReadLine();
+        Console.Clear();
+        if (resposta2 == "1")
         {
-    	   Console.WriteLine("Quantos pontos precisam para você pegar todos os prêmios?");
-    	   total_points = Convert.ToDouble(Console.ReadLine());
-           Console.Clear();
-    	   Console.WriteLine("Quantos pontos voce ja tem?");
-    	   your_points = Convert.ToDouble(Console.ReadLine());
-    	   rest_points = total_points - your_points;
-    	   total_hours = rest_points / 60;
-           Console.Clear();
-           Console.WriteLine("Ok eu analisei tudo e");
-    	   Console.WriteLine("Ainda faltam " + rest_points + " pontos");
-    	   Console.WriteLine("Ainda faltam " + total_hours + " horas");
-           Console.WriteLine("voltar para o menu? y/n");
-           resposta2 = Console.ReadLine();
-           Console.Clear();
-           if (resposta2 == "y")
-           {
-               Main();
-           }
+          Main();
         }
-        else if (resposta == "n")
-        {
-            Console.Clear();
-        }
-        else
-        {
-            Main();    
-        }
-	}
+      }
+      else if (resposta == "2")
+      {
+        Console.Clear();
+      }
+      else
+      {
+        Main();    
+      }
+  }
 }
